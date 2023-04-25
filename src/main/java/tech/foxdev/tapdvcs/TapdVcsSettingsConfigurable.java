2e65e126d7c241d21fc5f73cc64834c8827384bf
Foxjojo <foxjojo@outlook.com>
2023-04-26 00:22:32 +0800

    整理扫码登录流程

--- src/main/java/tech/foxdev/tapdvcs/TapdVcsSettingsConfigurable.java
+++ src/main/java/tech/foxdev/tapdvcs/TapdVcsSettingsConfigurable.java
@@ -11,6 +11,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.util.NlsContexts;
+import com.intellij.ui.components.JBPanel;
 import groovy.json.JsonBuilder;
 import kotlinx.serialization.json.Json;
 import net.minidev.json.JSONObject;
@@ -20,6 +21,8 @@ import org.jsoup.Jsoup;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.Console;
 import java.io.IOException;
@@ -29,9 +32,15 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpTimeoutException;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -43,6 +52,7 @@ public class TapdVcsSettingsConfigurable implements Configurable {
 
     private JPanel loginPanel;
     private JLabel qr;
+    private JLabel qrStatus;
     private JPanel projectPanel;
     private JLabel loginUserName;
     private JComboBox projects;
@@ -59,27 +69,50 @@ public class TapdVcsSettingsConfigurable implements Configurable {
 
             myMainComponent = new JPanel();
             var setData = TapdVcsSettingsState.getInstance();
-            projectPanel = new JPanel(new FlowLayout());
-            loginUserName = new JLabel(setData.userName);
+            projectPanel = new JPanel(new GridLayout(5, 1));
+            loginUserName = new JLabel(setData.userName, JLabel.CENTER);
             projects = new ComboBox();
+            logout = new JButton("Logout");
+            logout.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    loginPanel.setVisible(true);
+                    projectPanel.setVisible(false);
+                    try {
+                        GetQR();
+                    } catch (JsonProcessingException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+            projectPanel.add(new JLabel("CurUser", JLabel.CENTER));
             projectPanel.add(loginUserName);
+            projectPanel.add(new JLabel("CurProject", JLabel.CENTER));
             projectPanel.add(projects);
-            InitProjectPanel();
-
+            projectPanel.add(logout);
             myMainComponent.add(projectPanel);
 
 
-//            loginPanel = new JPanel(new FlowLayout());
-//            qr = new JLabel();
-//            qr.setSize(200, 200);
-//            loginPanel.add(qr);
-//            try {
-//                GetQR();
-//            } catch (JsonProcessingException e) {
-//                throw new RuntimeException(e);
-//            }
+            loginPanel = new JPanel(new GridLayout(5, 1));
+            qrStatus = new JLabel("Please use the WeCom (WeChat Work) mobile app to scan the QR code to login", JLabel.CENTER);
+            qr = new JLabel();
+            loginPanel.add(qr);
+            loginPanel.add(qrStatus);
 
-
+            myMainComponent.add(loginPanel);
+            if (setData.cookieExpirationDate == 0 || setData.cookieExpirationDate < System.currentTimeMillis()) {
+                loginPanel.setVisible(true);
+                projectPanel.setVisible(false);
+                try {
+                    GetQR();
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                loginPanel.setVisible(false);
+                projectPanel.setVisible(true);
+                InitProjectPanel();
+            }
 
 
         }
@@ -104,26 +137,32 @@ public class TapdVcsSettingsConfigurable implements Configurable {
 
     private void InitProjectPanel() {
 
-        final DefaultComboBoxModel projectIDs = new DefaultComboBoxModel();
 
         TapdVcsSettingsState settings = TapdVcsSettingsState.getInstance();
         String nowSelectID = settings.projectID;
         String cookie = settings.cookie;
+
         String url = "https://www.tapd.cn/company/participant_projects";
         Get(url, HttpResponse.BodyHandlers.ofString(), cookie).thenApply(HttpResponse::body).thenAccept((body) -> {
+            int index = 0;
+            DefaultComboBoxModel<Project> projectIDs = new DefaultComboBoxModel<Project>();
             var doc = Jsoup.parse((String) body);
             String userName = doc.getElementsByClass("dropdown-toggle").first().attr("title");
             loginUserName.setText(userName);
             var allProject = doc.getElementsByClass("project-list opened-project-list");
             for (int i = 0; i < allProject.size(); i++) {
                 var id = allProject.get(i).getElementsByTag("a").attr("href").substring(20);
-                var name = allProject.get(i).getElementsByTag("div").text();
+                var name = allProject.get(i).text();
                 Project project = new Project(name, id);
+                if (id.equals(nowSelectID)) {
+                    index = i;
+                }
                 projectIDs.addElement(project);
             }
-
+            projects.setModel(projectIDs);
+            projects.setSelectedIndex(index);
         });
-        projects.setModel(projectIDs);
+
     }
 
     private static HttpClient httpClient = HttpClient.newBuilder().build();
@@ -175,14 +214,30 @@ public class TapdVcsSettingsConfigurable implements Configurable {
                 Get(reUrl, HttpResponse.BodyHandlers.ofString()).thenAcceptAsync((response) -> {
                     var cookie = response.headers().allValues("set-cookie");
                     var setData = TapdVcsSettingsState.getInstance();
+                    //tapdsession=168242677222a7e38377dcd8b8113a69b9f281d0688c4cf37f2ec156e07bb3956c5da82b90; expires=Tue, 02-May-2023 12:46:12 GMT; Max-Age=604800; path=/; domain=.tapd.cn; HttpOnly
+                    var time = cookie.get(0).split(";")[1].substring(9).replace("GMT", "");
+
+                    var date = new SimpleDateFormat("EEE, dd-MMM-yyyy HH:mm:ss ");
+                    date.setDateFormatSymbols(new DateFormatSymbols(new Locale("en-US")));
                     String str = cookie.stream().collect(Collectors.joining(";"));
                     setData.cookie = str;
+                    try {
+                        setData.cookieExpirationDate = date.parse(time).getTime();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    loginPanel.setVisible(false);
+                    projectPanel.setVisible(true);
+                    InitProjectPanel();
+
                 });
                 return;
             }
 
             if (status.equals("QRCODE_SCAN_ING")) {
                 CheckQrCallback(appid, key, true);
+                qrStatus.setText("Scan Successful!  Please Confirm");
             } else {
                 CheckQrCallback(appid, key, false);
             }
@@ -227,6 +282,7 @@ public class TapdVcsSettingsConfigurable implements Configurable {
     @Override
     public void apply() {
         var setData = TapdVcsSettingsState.getInstance();
+        setData.projectID = ((Project) projects.getSelectedItem()).id;
 //        setData.projectID = myProjectIDField.getText();
 //        setData.cookie = myCookieField.getText();
     }
