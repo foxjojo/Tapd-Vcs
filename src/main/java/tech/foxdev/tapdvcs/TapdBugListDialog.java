9c22cd4939ac9ec397ea09d693187abeee7cfdd0
foxjojo <35793308+foxjojo@users.noreply.github.com>
2023-05-29 18:28:21 +0800

    改为异步加载数据 ps目前窗口大小不正确

--- src/main/java/tech/foxdev/tapdvcs/TapdBugListDialog.java
+++ src/main/java/tech/foxdev/tapdvcs/TapdBugListDialog.java
@@ -6,32 +6,34 @@ import org.jsoup.Jsoup;
 
 import javax.swing.*;
 import java.awt.event.*;
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 
 public class TapdBugListDialog extends JDialog {
     public static String TapdComment;
-    private JPanel contentPane;
+    private JPanel contentPanel;
     private JButton buttonOK;
     private JButton buttonCancel;
     private JList bugList;
     private JTextArea comment;
+    private JLabel loadTip;
+    private JPanel main;
 
     private static CommitMessageI commitPanel;
     private static Project project;
 
     public TapdBugListDialog() {
-        setContentPane(contentPane);
+        setContentPane(contentPanel);
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
         bugList.setCellRenderer(new CheckboxListCellRenderer());
-
-
+        contentPanel.setSize(550,150);
+        contentPanel.updateUI();
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onOK();
@@ -53,12 +55,15 @@ public class TapdBugListDialog extends JDialog {
         });
 
         // call onCancel() on ESCAPE
-        contentPane.registerKeyboardAction(new ActionListener() {
+        contentPanel.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onCancel();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
+
+        main.setVisible(false);
+        loadTip.setVisible(true);
         GetAllBug();
 
     }
@@ -120,43 +125,43 @@ public class TapdBugListDialog extends JDialog {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-        HttpResponse<String> response = null;
-        try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            new TipDialog(project, "Network Error").show();
-            dispose();
-            throw new RuntimeException(e);
-        }
-        // HTTP允许重复的Header，因此一个Header可对应多个Value:
-        //System.out.println(response.body());
-        var doc = Jsoup.parse(response.body());
-        var allBugs = doc.getElementsByClass("tfl-editable");
-        final DefaultListModel bugs = new DefaultListModel();
 
-        var ownName = doc.getElementsByClass(" left-tree-brick nav-iconbtn dropdown user").first().getElementsByTag("a").first().attr("title");
-        VcsHandler.ClearData();
-        for (int i = 0; i < allBugs.size(); i++) {
-            var bug = allBugs.get(i).getElementsByClass("card-title content-cardtitle namecol preview-title J-worktablePreview").first();
-            var data = new TapdBugData();
-            data.DisplayName = bug.attr("title");
-            data.Url = bug.attr("href");
-            data.ID = bug.attr("data-entityid");
-            var nameItem = allBugs.get(i).getElementById("td_bug_reporter_" + data.ID);
-            if( nameItem== null){
-               continue;
+        CompletableFuture<HttpResponse<String>> response = null;
+        response = httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        response.thenApply(HttpResponse::body).thenAccept((body) -> {
+            // HTTP允许重复的Header，因此一个Header可对应多个Value:
+            //System.out.println(response.body());
+            var doc = Jsoup.parse(body);
+            var allBugs = doc.getElementsByClass("tfl-editable");
+            final DefaultListModel bugs = new DefaultListModel();
+
+            var ownName = doc.getElementsByClass(" left-tree-brick nav-iconbtn dropdown user").first().getElementsByTag("a").first().attr("title");
+            VcsHandler.ClearData();
+            for (int i = 0; i < allBugs.size(); i++) {
+                var bug = allBugs.get(i).getElementsByClass("card-title content-cardtitle namecol preview-title J-worktablePreview").first();
+                var data = new TapdBugData();
+                data.DisplayName = bug.attr("title");
+                data.Url = bug.attr("href");
+                data.ID = bug.attr("data-entityid");
+                var nameItem = allBugs.get(i).getElementById("td_bug_reporter_" + data.ID);
+                if (nameItem == null) {
+                    continue;
+                }
+                var createName = nameItem.getElementsByTag("span").first().wholeText();
+                data.CreateName = createName;
+                data.OwnName = ownName;
+                bugs.addElement(data);
+
             }
-            var createName = nameItem.getElementsByTag("span").first().wholeText();
-            data.CreateName = createName;
-            data.OwnName = ownName;
-            bugs.addElement(data);
 
-        }
+            bugList.setModel(bugs);
+            main.setVisible(true);
+            loadTip.setVisible(false);
+        });
 
-        bugList.setModel(bugs);
+
     }
+
 
 
 }
